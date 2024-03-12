7077bc6c5f86ff6ca3a1a3d1b83d5b454089c890
Vinicius Pereira <vinniegcp@gmail.com>
2024-03-12 20:15:37 -0300

    vamos

--- assets/script.js
+++ assets/script.js
@@ -2,15 +2,47 @@ const email = document.getElementById("email");
 const form = document.querySelector("form");
 
 form.addEventListener("submit", (event) => {
-  if (email.value === "") {
-    alert("Preencha o campo de email");
-  }
-
   event.preventDefault();
+  checkForm();
 });
 
-function validatorEmail(email) {
-  let emailPattern =
-    /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
-  return emailPattern.test(email);
+email.addEventListener("blur", () => {
+  checkInputEmail();
+});
+
+function checkInputEmail() {
+  const emailValue = email.value;
+
+  if (emailValue === "") {
+    errorInput(email, "Digite seu email!");
+  } else {
+    const formItem = email.parentElement;
+    formItem.className = "form-content";
+  }
 }
+
+function checkForm() {
+  checkInputEmail();
+
+  const formItems = document.querySelectorAll(".form-content");
+  const isValid = Array.from(formItems).every((item) => {
+    return item.className === "form-content";
+  });
+
+  if (isValid) {
+    window.location.href = "./assets/page/obrigado.html";
+  }
+}
+
+function errorInput(input, message) {
+  const formItem = input.parentElement;
+  const textMessage = formItem.querySelector("a");
+  textMessage.innerText = message;
+  formItem.className = "form-content error";
+}
+
+// Atualiza a mensagem de confirmação com o email de inscrição
+/*const emailValue = email.value;
+document.getElementById(
+  "confirmation-message"
+).textContent = `Um email de confirmação de inscrição foi enviado para ${emailValue}.`;*/
